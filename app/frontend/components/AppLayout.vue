0434650b02430ccb855f5c64dacc1be72df9c2ba
Emerson Freitas <mersonsi@gmail.com>
2022-12-24 14:18:40 -0300

    build: iniciando o projeto

---
+++ app/frontend/components/AppLayout.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div>
+    <NavBar/>
+  </div>
+</template>
+<script>
+import NavBar from './NavBar.vue'
+export default {
+  components: {
+    'NavBar': NavBar
+  },
+}
+</script>
+<style lang="">
+  
+</style>
