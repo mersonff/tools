6208d2fbc095562def40c51557050df5ce3c2a58
Emerson Freitas <mersonsi@gmail.com>
2022-12-27 17:19:48 -0300

    feat: model de ferramenta

---
+++ app/models/tool.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Tool < ApplicationRecord
+  belongs_to :place
+
+  enum kind: { air_conditioning: 0, pc: 1, monitor: 2, mouse: 3, keyboard: 4, tv: 5, router: 6, coffee_maker: 7 },
+    _default: 0
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :brand, presence: true
+  validates :kind, presence: true
+  validates :code, presence: true, numericality: { only_integer: true }
+end
